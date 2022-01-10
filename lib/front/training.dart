2ee9a20c4c8f06ad2d34392c81e7cdaba4715910
jonbee0427 <greathyun0427@gmail.com>
2022-01-10 12:00:59 +0900

    JH/0110/file modified

--- lib/front/training.dart
+++ lib/front/training.dart
@@ -54,7 +54,6 @@ class _TrainingState extends State<Training> {
                     TextButton(
                       onPressed: () {
                         if(widget.bluetoothServices != null)
-                          // Navigator.push(context, MaterialPageRoute(builder: (context) => Jumpingjack(bluetoothServices: widget.bluetoothServices)));
                           Navigator.push(context, MaterialPageRoute(builder: (context) => Jumpingjack(bluetoothServices: widget.bluetoothServices)));
                         if (widget.bluetoothServices == null)
                           SchedulerBinding.instance!.addPostFrameCallback((_) {
@@ -97,8 +96,7 @@ class _TrainingState extends State<Training> {
                     // SizedBox(height: 5,),
                     TextButton(
                       onPressed: () {
-                        if(widget.bluetoothServices != null)
-                          Navigator.push(context, MaterialPageRoute(builder: (context) => RiveTest(bluetoothServices: widget.bluetoothServices)));
+                        // if(widget.bluetoothServices != null)
                       },
                       child: Image.asset('crunch.png', width: MediaQuery.of(context).size.width*0.9,),
                     ),
@@ -144,110 +142,6 @@ class _TrainingState extends State<Training> {
           );
         }
         )
-    );
-  }
-}
-
-class RiveTest extends StatefulWidget {
-  final List<BluetoothService>? bluetoothServices;
-  RiveTest({this.bluetoothServices});
-
-  @override
-  _RiveTestState createState() => _RiveTestState();
-}
-
-class _RiveTestState extends State<RiveTest> {
-  final Map<Guid, List<int>> readValues = new Map<Guid, List<int>>();
-  String gesture = "";
-  // ignore: non_constant_identifier_names
-  int gesture_num = 0;
-
-  late RiveAnimationController _controller;
-  late RiveAnimationController _openController;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = SimpleAnimation('Idle');
-
-    _openController = OneShotAnimation(
-      'Jumpingjack',
-      autoplay: false,
-    );
-
-  }
-
-  ListView _buildConnectDeviceView() {
-    // ignore: deprecated_member_use
-    List<Container> containers = [];
-    for (BluetoothService service in widget.bluetoothServices!) {
-      // ignore: deprecated_member_use
-      List<Widget> characteristicsWidget = [];
-
-      for (BluetoothCharacteristic characteristic in service.characteristics) {
-        if (characteristic.properties.notify) {
-          characteristic.value.listen((value) {
-            readValues[characteristic.uuid] = value;
-          });
-          characteristic.setNotifyValue(true);
-        }
-        if (characteristic.properties.read && characteristic.properties.notify) {
-          setnum(characteristic);
-        }
-      }
-      containers.add(
-        Container(
-          child: ExpansionTile(
-              title: Center(child:Text("블루투스 연결설정")),
-              children: characteristicsWidget),
-        ),
-      );
-    }
-
-    return ListView(
-      padding: const EdgeInsets.all(8),
-      children: <Widget>[
-        Container(
-          height: MediaQuery.of(context).size.height,
-          child: RiveAnimation.asset(
-            'assets/rive/move_jumpingjack.riv',
-            controllers: [_controller, _openController],
-          ),
-        ),
-      ],
-    );
-  }
-
-  Future<void> setnum(characteristic) async {
-    var sub = characteristic.value.listen((value) {
-      setState(() {
-        readValues[characteristic.uuid] = value;
-        gesture = value.toString();
-        gesture_num = int.parse(gesture[1]);
-      });
-    });
-
-    if(gesture_num == 1) {
-      setState(() {
-        _openController.isActive = true;
-      });
-    }
-
-    await characteristic.read();
-    sub.cancel();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-          height: MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width,
-          decoration: BoxDecoration(
-              color: const Color(0xff37384E)
-          ),
-          child: _buildConnectDeviceView()
-      )
     );
   }
 }
