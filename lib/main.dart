cafc9d399f9e8b54da28a700177dfddac556ae1c
jonbee0427 <greathyun0427@gmail.com>
2022-07-05 14:00:57 +0900

    JH/0705/I-corps new customers interview conducted

--- lib/main.dart
+++ lib/main.dart
@@ -86,5 +86,5 @@ class _SplashScreenState extends State<SplashScreen> with TickerProviderStateMix
   }
 }
 
-// 22/07/04 Monday.
-// (part 2) I-corps customer interview conducted & BMC system modified!
+// 22/07/05 Tuesday.
+// (part 3) I-corps customer interview conducted & BMC system modified!
